faaac96223512b95f971642e09729c4deaaa5303
Igor <SandQuattro@gmail.com>
2024-08-27 16:31:08 +0300

    added channels hell example

---
+++ concurrency/channels/channels_hell.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+type C chan C
+
+func main() {
+
+	var c = make(C, 1)
+	c <- c
+	for i := 0; i < 1000; i++ {
+		select {
+		case <-c:
+		case <-c:
+			c <- c
+		default:
+			fmt.Println(i)
+			return
+		}
+	}
+}
