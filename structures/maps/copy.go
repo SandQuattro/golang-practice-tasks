9cecc49b4f133d7484ee8ffd2692139c0c4e6021
Igor <SandQuattro@gmail.com>
2024-09-28 11:12:35 +0300

    copy added

--- structures/maps/copy.go
+++ structures/maps/copy.go
@@ -4,9 +4,10 @@ import "fmt"
 
 func main() {
 	m1 := map[string]int{"test": 1}
-	fmt.Printf("m2 pointer: %p, value: %v\n", m1, m1)
-	m2 := copyMap(m1)
-	fmt.Printf("m2 pointer: %p, value: %v\n", m2, m2)
+	fmt.Printf("m1 pointer: %p, m1 value: %v\n", m1, m1)
+	m2 := copyMap2(m1)
+	fmt.Printf("m1 pointer: %p, m1 value: %v\n", m2, m2)
+	fmt.Printf("m2 pointer: %p, m2 value: %v\n", m2, m2)
 }
 
 func copyMap(m map[string]int) map[string]int {
@@ -15,4 +16,9 @@ func copyMap(m map[string]int) map[string]int {
 		res[k] = v
 	}
 	return res
+}
+
+func copyMap2(m map[string]int) map[string]int {
+	m["test2"] = 2
+	return m
 }
