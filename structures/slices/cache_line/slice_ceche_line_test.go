bcc247b6763620387452f403277eef7b385f4a4e
Igor <SandQuattro@gmail.com>
2024-09-28 09:41:49 +0300

    added slice cache line performance

---
+++ structures/slices/cache_line/slice_ceche_line_test.go
@@ -0,0 +1,45 @@
+package cache_line
+
+import (
+	"testing"
+)
+
+const size = 64
+
+func sliceIterator(sl []int) {
+	var cnt int
+	for i := 0; i < size; i++ {
+		cnt += sl[i]
+	}
+}
+
+func mapIterator(m map[int]int) {
+	var cnt int
+	for i := 0; i < size; i++ {
+		cnt += m[i]
+	}
+}
+
+func BenchmarkSliceReading(b *testing.B) {
+	sl := make([]int, size)
+	for i := 0; i < size; i++ {
+		sl[i] = i
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		sliceIterator(sl)
+	}
+}
+
+func BenchmarkMapReading(b *testing.B) {
+	m := make(map[int]int, size)
+	for i := 0; i < size; i++ {
+		m[i] = i
+	}
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		mapIterator(m)
+	}
+}
