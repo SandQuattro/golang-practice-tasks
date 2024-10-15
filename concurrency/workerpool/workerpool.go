e670ec5c778f7f8740fcf70cf345742375a0c955
Igor <SandQuattro@gmail.com>
2024-10-15 20:23:42 +0300

    chore: improved worker pool

--- concurrency/workerpool/workerpool.go
+++ concurrency/workerpool/workerpool.go
@@ -21,42 +21,48 @@ func main() {
 	go func() {
 		for i := 0; i < 10; i++ {
 			site <- SiteData{fmt.Sprintf("url%d", i), fmt.Sprintf("content%d", i)}
-			time.Sleep(time.Duration(i) * time.Millisecond)
+			time.Sleep(time.Duration(i) * time.Millisecond * 10)
 		}
 		close(site)
 	}()
 
 	// Запуск worker-ов
-	data := processSiteData(site, runtime.NumCPU())
+	for data := range processSiteData(site, runtime.NumCPU()) {
+		// Выводим данные с сайтов
+		fmt.Println(data)
+	}
 
-	// Выводим данные с сайтов
-	fmt.Println(data)
 }
 
 // выгребаем данные сайтов из канала
-func processSiteData(c <-chan SiteData, numWorkers int) map[string]SiteData {
-	sites := make(map[string]SiteData)
+func processSiteData(c <-chan SiteData, numWorkers int) <-chan string {
+	sites := make(chan string, numWorkers)
 
-	fmt.Println("Starting workers:", numWorkers)
-
-	wg := sync.WaitGroup{}
-	for i := 0; i < numWorkers; i++ {
-		wg.Add(1)
-		i := i
-		go func() {
-			log.Printf("worker %d online", i+1)
-			defer wg.Done()
-			for {
-				val, ok := <-c
-				if !ok {
-					// канал закрыт, выходим
-					break
+	// оптимизация, мы сразу асинхронно вернем канал в main и начнем из него читать
+	go func() {
+		fmt.Println("Starting workers:", numWorkers)
+		wg := sync.WaitGroup{}
+		for i := 0; i < numWorkers; i++ {
+			wg.Add(1)
+			i := i
+			go func() {
+				log.Printf("worker %d online", i+1)
+				defer wg.Done()
+				for {
+					val, ok := <-c
+					if !ok {
+						// канал закрыт, выходим
+						break
+					}
+					log.Printf(">> worker %d did the job:%s", i+1, val)
+					sites <- val.Content
 				}
-				log.Printf(">> worker %d did the job:%s", i+1, val)
-				sites[val.URL] = val
-			}
-		}()
-	}
-	wg.Wait()
+			}()
+		}
+
+		wg.Wait()
+		close(sites)
+	}()
+
 	return sites
 }
